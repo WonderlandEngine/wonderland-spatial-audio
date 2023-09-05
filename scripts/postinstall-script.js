81929c37ecd058b17a869b7349757222b777373f
Timothy Hale <timothyh@tutanota.com>
2023-09-05 11:07:07 +0200

    hrtf: add review changes

--- scripts/postinstall-script.js
+++ scripts/postinstall-script.js
@@ -25,9 +25,9 @@ import {fileURLToPath} from 'url';
 const pathToScript = fileURLToPath(import.meta.url);
 /* We assume npm install is called on project root, since the root package.json resides there. */
 const projectRoot = process.cwd();
-const sourceFile = path.join(path.dirname(pathToScript), '..', 'hrtf', 'hrtf_128.bin.gz');
+const sourceFile = path.join(path.dirname(pathToScript), '..', 'hrtf', 'hrtf_128.bin');
 
-const destinationFile = path.join(projectRoot, 'static', 'hrtf_128.bin.gz');
+const destinationFile = path.join(projectRoot, 'static', 'hrtf_128.bin');
 const destinationFolder = path.join(projectRoot, 'static');
 
 if (!fs.existsSync(destinationFolder)) fs.mkdirSync(destinationFolder);
